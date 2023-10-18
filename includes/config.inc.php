77d0b1c69ab81e6cc3dcbf3f2e4baba540d358c8
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2023-10-18 18:59:08 +0200

    Add some new files about login and other functions

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -17,7 +17,12 @@ $dbjelszo='';
 
 $oldalak = array(	
 	'/' => array('fajl' => 'kezdolap', 'szoveg' => 'Kezdőlap', 'menun' => array(1, 1)),
+	'belepes' => array('fajl' => 'belepes', 'szoveg' => 'Belépés/Regisztráció', 'menun' => array(1, 0)),
+	'kilepes' => array('fajl' => 'kilepes', 'szoveg' => 'Kilépés', 'menun' => array(0, 1)),
+	'belep' => array('fajl' => 'belep', 'szoveg' => '', 'menun' => array(0, 0)),	
+	'regisztral' => array('fajl' => 'regisztral', 'szoveg' => '', 'menun' => array(0, 0)),
 );
+$hiba_oldal = array('fajl' => '404', 'szoveg' => 'A keresett oldal nem található!');
 
 $MAPPA = 'images/';
 $TIPUSOK = array('.jpg', '.png');
--- templates/pages/kezdolap.tpl.php
+++ templates/pages/kezdolap.tpl.php
@@ -2,5 +2,6 @@
 	<div class="inner">
 		<h1>Cuki</h1>
 		<p>Üdvözöljük a Cukrászdában</p>
+        <img src="images/kep01.jpg">
 	</div>
 </section>
