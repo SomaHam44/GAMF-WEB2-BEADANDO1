beff1720f9ac0a88f21f976172750ee4b2c53b1e
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2023-10-18 20:22:25 +0200

    Add new files about login and starting style

--- templates/pages/kezdolap.tpl.php
+++ templates/pages/kezdolap.tpl.php
@@ -1,7 +1,7 @@
 <section>
-	<div class="inner">
+	<div class="content">
 		<h1>Cuki</h1>
 		<p>Üdvözöljük a Cukrászdában</p>
-        <img src="images/kep01.jpg">
+        <img src="images/kep01.jpg" data-position="center center" width="490" height="500">
 	</div>
 </section>
