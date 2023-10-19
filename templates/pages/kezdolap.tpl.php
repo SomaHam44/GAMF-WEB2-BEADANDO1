32a5640ced225e406ed97530af749fe4deaf88c9
feketeand <afekete.andrea@gmail.com>
2023-10-19 23:12:39 +0200

    kezdőlap alakítás
    kep01.jpg háttérképnek

--- templates/pages/kezdolap.tpl.php
+++ templates/pages/kezdolap.tpl.php
@@ -1,7 +1,6 @@
 <section>
 	<div class="content">
 		<h1>Édes Élmény Cukrászda</h1>
-		<p>Üdvözöljük az Édes Élmény Cukrászdában</p>
-        <img src="images/kep01.jpg" data-position="center center" width="490" height="500">
+		<p>Üdvözöljük az Édes Élmény Cukrászdában</p>		        
 	</div>
 </section>
