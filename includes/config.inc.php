affcb21b265b736ab297a87d6f25bd15bf5527e8
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2023-10-20 17:47:47 +0200

    Új dolgok hozzáadása

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -17,12 +17,17 @@ $dbjelszo='';
 
 $oldalak = array(	
 	'/' => array('fajl' => 'kezdolap', 'szoveg' => 'Kezdőlap', 'menun' => array(1, 1)),
-    'hirek' => array('fajl' => 'hirek', 'szoveg' => 'Hírek', 'menun' => array(0, 1)),
+    'hiroldal' => array('fajl' => 'hiroldal', 'szoveg' => 'Hírek', 'menun' => array(0, 1)),
+	'soap' => array('fajl' => 'soap', 'szoveg' => 'SOAP', 'menun' => array(0, 1)),
+	'lekerdezes' => array('fajl' => 'lekerdezes', 'szoveg' => 'Lekérdezések', 'menun' => array(0, 1)),
 	'belepes' => array('fajl' => 'belepes', 'szoveg' => 'Belépés/Regisztráció', 'menun' => array(1, 0)),
 	'kilepes' => array('fajl' => 'kilepes', 'szoveg' => 'Kilépés', 'menun' => array(0, 1)),
 	'belep' => array('fajl' => 'belep', 'szoveg' => '', 'menun' => array(0, 0)),	
 	'regisztral' => array('fajl' => 'regisztral', 'szoveg' => '', 'menun' => array(0, 0)),
+	'hir' => array('fajl' => 'hir', 'szoveg' => '', 'menun' => array(0, 0)),
+	'uzenet' => array('fajl' => 'uzenet', 'szoveg' => '', 'menun' => array(0, 0)),
 );
+
 $hiba_oldal = array('fajl' => '404', 'szoveg' => 'A keresett oldal nem található!');
 
 $MAPPA = 'images/';
---
+++ templates/pages/lekerdezes.tpl.php
@@ -0,0 +1,4 @@
+<section>
+	<div class="content">
+		<h1>Lekérdezések</h1>
+</section>
