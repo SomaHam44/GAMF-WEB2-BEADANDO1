9bf631f2cfd5160f7bf94041bb43a7115f254b0c
feketeand <afekete.andrea@gmail.com>
2023-10-23 08:50:45 +0200

    mvc átalakítás

--- templates/pages/hiroldal.tpl.php
+++
@@ -1,8 +0,0 @@
-<section>
-	<div class="content">
-		<h1>Hírek</h1>
-        <p>Vélemények</p>	        
-	</div>
-    <label>Vélemény: </label>
-    <textarea></textarea>
-</section>
--- templates/pages/kezdolap.tpl.php
+++
@@ -1,6 +0,0 @@
-<section>
-	<div class="content">
-		<h1>Édes Élmény Cukrászda</h1>
-		<p>Üdvözöljük az Édes Élmény Cukrászdában</p>		        
-	</div>
-</section>
