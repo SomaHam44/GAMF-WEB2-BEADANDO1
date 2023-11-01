3ea29a183596382d30fd255f4d8283bb8985c32d
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2023-11-01 10:20:48 +0100

    Javítás

--- views/belepes_main.php
+++ views/belepes_main.php
@@ -1,5 +1,5 @@
 <h2>Belépés</h2>  
-<form action="<?= SITE_ROOT ?>?beleptet" method="post">
+<form action="<?= SITE_ROOT ?>beleptet" method="post">
     <fieldset>                  
         <label for="login">Felhasználó:</label><input type="text" name="login" id="login" required pattern="[a-zA-Z][\-\.a-zA-Z0-9_]{3}[\-\.a-zA-Z0-9_]+"><br>
         <label for="password">Jelszó:</label><input type="password" name="password" id="password" required pattern="[\-\.a-zA-Z0-9_]{4}[\-\.a-zA-Z0-9_]+"><br>
@@ -8,7 +8,7 @@
 </form>
 
 <h2>Regisztráció</h2>
-<form action = "<?= SITE_ROOT ?>?regisztral" method="post">
+<form action = "<?= SITE_ROOT ?>regisztral" method="post">
     <fieldset>              
         <label for="csaladi_nev">Vezetéknév:</label><input type="text" name="csaladi_nev" required><br><br>
         <label for="utonev">Utónév:</label><input type="text" name="utonev" required><br><br>
