8b95133180a70d441de39d1bf40a862a3a2a1ccb
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2023-11-02 18:32:28 +0100

    View és Model módosítások

---
+++ models/suti_model.php
@@ -0,0 +1,69 @@
+<?php
+class Suti 
+{
+    private $id;
+    private $nev;
+    private $tipus;
+    private $dijazott;
+    private $egysegar;
+    private $egyseg;
+
+    public function _construct($id, $nev, $tipus, $dijazott)
+    {
+        $this->id = $id;
+        $this->nev = $nev;
+        $this->tipus = $tipus;
+        $this->dijazott = $dijazott;
+        $this->egysegar = $egysegar;
+        $this->egyseg = $egyseg;
+    }
+
+
+    public function getId() : ?int
+    {
+        return $this->id;
+    }
+
+    public function getNev() : ?string
+    {
+        return $this->nev;
+    }
+
+    public function getTipus() : ?string
+    {
+        return $this->tipus;
+    }
+
+    public function getDijazott() : ?int
+    {
+        return $this->dijazott;
+    }
+
+    public function setNev($nev)
+    {
+        $this->nev = $nev;
+    }
+
+    public function setTipus($tipus)
+    {
+        $this->tipus = $tipus;
+    }
+
+    public function setDijazott($dijazott)
+    {
+        $this->dijazott = $dijazott;
+    }
+
+    public function setEgysegar($egysegar)
+    {
+        $this->egysegar = $egysegar;
+    }
+
+    public function setEgyseg($egyseg)
+    {
+        $this->egyseg = $egyseg;
+    }
+
+}
+
+?>
--- views/page_main.php
+++ views/page_main.php
@@ -4,7 +4,6 @@
         <meta charset="utf-8">
         <title>Édes Élmény Cukrászda</title>
         <link rel="stylesheet" type="text/css" href="<?php echo SITE_ROOT?>css/main_style.css">
-        <!--<link rel="stylesheet" type="text/css" href="<?php echo SITE_ROOT?>css/main_old.css">--->
         <?php if($viewData['style']) echo '<link rel="stylesheet" type="text/css" href="'.$viewData['style'].'">'; ?>
     </head>
     <body>
@@ -18,7 +17,7 @@
             <?php echo Menu::getMenu($viewData['selectedItems']); ?>
         </nav>
         <aside>
-                <p>Lorem ipsum....</p>
+                <p>Expressz tortarendelés: 0620/555555</p>
         </aside>
         <section>
             <?php if($viewData['render']) include($viewData['render']); ?>
