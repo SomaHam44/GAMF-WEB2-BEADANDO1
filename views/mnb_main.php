ab63c6b053319de63e68285b50a1e810af7a2ce1
feketeand <afekete.andrea@gmail.com>
2023-11-05 22:18:00 +0100

    deviza lista megvan

--- views/mnb_main.php
+++ views/mnb_main.php
@@ -1,27 +1,30 @@
 
 <pre>
 <?php
+
     try {
         $client = new SoapClient("http://www.mnb.hu/arfolyamok.asmx?WSDL");
-        echo "<br>GetCurrentExchangeRates()<br>";
-// (array): itt egy asszociatív tömbbé konvertálja:
-        $tömb = (array)simplexml_load_string($client->GetCurrentExchangeRates()->GetCurrentExchangeRatesResult);
-        echo $tömb['Day']['date']."<br>";
-        echo  $tömb['Day']['2'];
-        print_r($tömb);
-
-        echo "<br>GetInfo()<br>";
-        $tömb = (array)simplexml_load_string($client->GetInfo()->GetInfoResult);
-        print_r($tömb['Currencies']);
-        
-
-        echo "<br>GetCurrencies()<br>";
-        $tömb = (array)simplexml_load_string($client->GetCurrencies()->GetCurrenciesResult);
-        print_r($tömb);
-
-        echo "<br>GetDateInterval()<br>";
-        $tömb = (array)simplexml_load_string($client->GetDateInterval()->GetDateIntervalResult);
-        print_r($tömb);
+        echo "<br>Aktuális deviza árfolyamok<br>";       
+        $response_stdclass = $client->GetCurrentExchangeRates();
+        $xml = $response_stdclass->GetCurrentExchangeRatesResult;        
+        $parser=xml_parser_create();        
+        xml_parse_into_struct($parser, $xml, $ertekek);?>
+        <table>
+        <tr>
+                <th>Deviza</th>
+                <th>Egység</th>
+                <th>Árfolyam (HUF)</th>
+        <?php for($i=0; $i<count($ertekek); $i++){
+            if($ertekek[$i]['tag'] == 'RATE'){?>            
+            <tr>
+                <td><?php echo ($ertekek[$i]['attributes']['CURR']);?></td>
+                <td><?php echo ($ertekek[$i]['attributes']['UNIT']);?></td>
+                <td><?php echo ($ertekek[$i]['value'])."<br>";?></td>
+            </tr> <?php                      
+            }
+        }?>
+        </table>        
+        <?php                   
 
     } catch (SoapFault $e) {
         var_dump($e);
