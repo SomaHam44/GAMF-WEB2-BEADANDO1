a6d8dc10361d7c1af601035118bbeb4fd5526399
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2023-11-15 21:09:29 +0100

    Fájl módosítás

--- soapKliens.php
+++ soapKliens.php
@@ -1,16 +1,3 @@
 <?php
-	//ini_set("default_socket_timeout", 5000);
-   $options = array(
-   "location" => "http://localhost/web2/soapSzerver.php",
-   "uri" => "http://localhost/web2/soapSzerver.php",
-   'keep_alive' => false,
-    //'trace' =>true,
-    //'connection_timeout' => 5000,
-    //'cache_wsdl' => WSDL_CACHE_NONE,
-   );		
-   try {
-	$kliens = new SoapClient(null, $options);	
-   } catch (SoapFault $e) {
-		var_dump($e);
-   }
+	
 ?>
