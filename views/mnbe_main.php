56586afb6095b8e8d77cd84687e81b32ff6b3077
feketeand <afekete.andrea@gmail.com>
2023-11-23 21:30:08 +0100

    Update mnbe_main.php

--- views/mnbe_main.php
+++ views/mnbe_main.php
@@ -1,29 +1,31 @@
-<?php $devizak=$viewData;
-?>
-<p>Lekérdezhető devizák:
-<?php
-$db=count($devizak)-2; 
-echo ($db)." db";?><br><?php
-echo "<select>";
-for ($i=2;$i<$db-1;$i++){
-    echo "<option>". $devizak[$i] . "</option>";
-}
-echo "</select>";
-//echo $devizak[$db-2];
-?><br>
-<p> Lekérdezhető időintervallum:</p>
-<?php
+<h2>Devizapárok lekérdezése adott időszakban:</h2> 
 
+<?php $devizak=$viewData;
+$db=count($devizak)-2; 
+?><br>
+<p> Lekérdezhető időintervallum:
+<?php
 $date1=$devizak[0];
 $date2=$devizak[1];
 echo $date1." - ".$date2;
-?>
+?><br><br></p>
 
-<h2>Devizapárok lekérdezése adott időszakban:</h2>  
+ 
 <form action="<?= SITE_ROOT ?>mnb2" method="post">
-    <fieldset>                  
-        <label for="v1">1. deviza:</label><input type="text" name="d1" id="d1" required><br>
-        <label for="v2">2. deviza: </label><input type="text" name="d2" id="d2" required ><br>
+    <fieldset>
+        <br>                 
+        <label for="d1">1. deviza:</label><select name="d1" id="d1">
+        <?php for ($i=3;$i<$db-1;$i++){?>
+            <option value="<?php echo $devizak[$i]?>"> <?php echo $devizak[$i]?> </option>
+        <?php }?>
+        </select>
+        <br><br>
+        <label for="d2">2. deviza: </label><select name="d2" id="d2">
+        <?php for ($i=3;$i<$db-1;$i++){?>
+            <option value="<?php echo $devizak[$i]?>"> <?php echo $devizak[$i]?> </option>
+        <?php }?>
+        </select>
+        <br>
         <label for="datum1">Kezdő dátum</label><input type="date" name="datum1" id="datum1"  required><br>
         <label for="datum2">Végdátum</label><input type="date" name="datum2" id="datum2"  required><br>    
         <input type="submit" value="Lekérdez">
